294dd2ec941f2099c6c05cd7c7c9d34ed98d6f31
Kevin Lopez <kevin_lopez@live.com>
2021-02-03 15:08:41 -0600

    changes cors origin to live react app url

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ const usersRouter = require ('./users/usersRouter')
 const cartsRouter = require('./carts/cartsRouter')
 
 server.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://wsky.vercel.app',
     credentials: true
 }))
 server.use(session({

356f952e8aba44392e5a90e3ad472e3eb63e49d9
Kevin Lopez <kevin_lopez@live.com>
2021-02-03 18:01:17 -0600

    .

--- knexfile.js
+++ knexfile.js
@@ -34,18 +34,13 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    useNullAsDefault: true
 
 };

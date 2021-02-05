034a23d57c5e66eed99dd07745f3efce0d4fea80
Kevin Lopez <kevin_lopez@live.com>
2021-02-05 13:24:36 -0600

    .

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ const usersRouter = require ('./users/usersRouter')
 const cartsRouter = require('./carts/cartsRouter')
 
 server.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://infinite-refuge-27306.herokuapp.com',
     credentials: true
 }))
 // server.enable('trust proxy')

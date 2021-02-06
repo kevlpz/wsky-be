09142f69cdd43444349feef7ab8e0938838ecf8f
Kevin Lopez <kevin_lopez@live.com>
2021-02-05 18:10:31 -0600

    .

--- knexfile.js
+++ knexfile.js
@@ -35,9 +35,9 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    },
+    // ssl: {
+    //   rejectUnauthorized: false
+    // },
     migrations: {
       directory: './data/migrations'
     },
